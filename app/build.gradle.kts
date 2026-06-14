967ab86e177fb401c971868ae8588ad57b5a6838
Christoph Thelen <thelen.christoph@gmail.com>
2026-06-14 18:48:11 +0200

    Use new logo as icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "de.hardtthelen.trainerapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
