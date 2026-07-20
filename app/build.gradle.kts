cfcd345672c420a1c9b70df3cbd1a59a7253e7c7
Christoph Thelen <thelen.christoph@gmail.com>
2026-07-20 18:51:30 +0200

    Update version for build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "de.hardtthelen.trainerapp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
